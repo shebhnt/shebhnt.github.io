e95b4cded5910e84ae6d50f4da3f73f147fad5d2
shebhnt <shelbey.hunt@sim.org>
2017-03-28 16:37:31 -0400

    Added contact, hire, and about pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 // 
 var endOfIntro = 0;
 var done = 1;
+var width = $(window).width();
+
 var showText = function (target, message, index, interval) {   
 	done = 0;
   if (index < message.length) {
@@ -12,18 +14,76 @@ var showText = function (target, message, index, interval) {
 	}
 };
 
+function turnOnOptions() {
+	
+	$( "#Options" ).css('display', 'block');
+	var items = $('.items');
+	var i = 0;
+	for (i = 0; i < items.length; i++) {
+		var item = $(items[i]);
+		item.animate({
+			opacity: 1
+		}, 1000);
+	}
+}
+
 function setUpOptions() {
 	$( '#introHelper' ).animate({
 		opacity: 0
 	}, 500, function() {
 		$( "#CenterText" ).animate({
-		height: "40vh"
-	}, 2000, function(){});
-	$( '#introText' ).animate({
-		fontSize: '75px'
-	}, 2000, function(){});
+			height: "30vh",
+			fontSize: '75px'
+		}, 2000, function(){			
+			turnOnOptions();
+		});
 	});
 	
+	
+}
+
+function goToAbout() {
+	$('body').removeClass().addClass('about');
+	$('.line').addClass('white');
+	$('.content').css('opacity', '0');
+	$('.myWorkItem').css('opacity', '0');
+	$('.hireMeItem').css('opacity', '0');
+	$('.contactMeItem').css('opacity', '0');
+	$('.aboutMeItem').animate({
+		opacity: 0
+	}, 2000, function(){
+		window.location.href = 'about.html';
+	});
+	
+}
+
+function goToContact() {
+	$('body').removeClass().addClass('contact');
+	$('.line').addClass('white');
+	$('.content').css('opacity', '0');
+	$('.myWorkItem').css('opacity', '0');
+	$('.hireMeItem').css('opacity', '0');
+	$('.aboutMeItem').css('opacity', '0');
+	$('.contactMeItem').animate({
+		opacity: 0
+	}, 2000, function(){
+		window.location.href = 'contact.html';
+	});
+	
+}
+
+function goToHire() {
+	$('body').removeClass().addClass('hire');
+	$('.line').removeClass('white');
+	$('.content').css('opacity', '0');
+	$('.myWorkItem').css('opacity', '0');
+	$('.aboutMeItem').css('opacity', '0');
+	$('.contactMeItem').css('opacity', '0');
+	$('.hireMeItem').animate({
+		opacity: 0
+	}, 2000, function(){
+		window.location.href = 'hire.html';
+	});
 	
 }
 
@@ -41,14 +101,22 @@ function nextText() {
 	console.log(curText.text());
 	curText.css('display', 'block');*/
 	curText.text('');
-	showText(curText, nexText.text(), 0, 100);
+	showText(curText, nexText.text(), 0, 50);
 	nexText.removeClass('active');
 	newText.addClass('active');
 	done = 0;
-	console.log(newText);
+	
 }
 
+
+
 $( document ).ready(function() {
+	$('.content').animate({
+		opacity: 1
+	}, 1000);
+	if ($('body').hasClass('about') || $('body').hasClass('hire') || $('body').hasClass('contact')) {
+		turnOnOptions();
+	}
 	document.body.onkeyup = function(e){
 		if(e.keyCode === 32){
 			if ( endOfIntro === 0 && done === 1 ) {
