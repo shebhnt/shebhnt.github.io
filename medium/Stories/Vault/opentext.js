ef8f54f707189ec40808ce2cb1e859e7ec2ff757
shebhnt <shelbey.hunt@sim.org>
2017-04-19 16:39:08 -0400

    Updated success text

--- medium/Stories/Vault/opentext.js
+++ medium/Stories/Vault/opentext.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 function openedVault(y, x) {
-	if (y === '12345') {
-		x.text('you opened the vault');
+	if (y === '46321') {
+		x.text('You open the vault and escape. You set out to catch up to the one who stabbed you in the back. There will be hell to pay!');
 	} else {
 		x.text('incorrect password');
 	}
